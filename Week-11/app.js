cfb986bc5cfcc4293ad26a9beb19f115e19fff9e
jeff1haupt <jhaupt@southbendin.gov>
2022-06-10 21:08:27 -0400

    updated class code

--- Week-11/app.js
+++ Week-11/app.js
@@ -15,20 +15,44 @@ $( document ).ready( () => {
 
     let array = []
     let turn = true;
+    let turnNumber = 1;
     for ( let i = 0; i < 9; i++ ) {
         let td = $(`#${i+1}`);
         td.on('click', () => {
             if (turn) {
                 td.text('X').addClass('XandO');
                 turn = false;
+                turnCounter();
             } else {
                 td.text('O').addClass('XandO');
                 turn = true;
+                turnCounter();
             }
             
         });
         array.push(td);
     }
+
+    function turnCounter() {
+        if ( turnNumber < 5 ) {
+            turnNumber++;
+        } else {
+            if ( checkForWin(turnNumber) ) {
+                // do something to show who won
+            }
+            turnNumber++;
+        }
+    }
+
+    function checkForWin(x) {
+        console.log('Turn Number greater than 5');
+        if ( x > 9 ) {
+            return false; //
+        } else {
+            // do something with the array we created earlier 
+        }
+    }
+    
     
     
 
